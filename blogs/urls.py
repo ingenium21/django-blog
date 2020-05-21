ef48ffdf56faa249d4c010bf9acf4ec410963c08
Renato Regalado <renato.regalado@gmail.com>
2020-05-21 15:41:08 -0500

    added list of posts, and a single post page

---
+++ blogs/urls.py
@@ -0,0 +1,12 @@
+"""Defines URL patterns for blogs"""
+from django.urls import path
+
+from . import views
+
+app_name = 'blogs'
+urlpatterns = [
+    #home page
+    path('', views.index, name='index'),
+    path('posts/', views.posts, name='posts'),
+    path('posts/<int:post_id>', views.post, name='post')
+]
--- blogs/views.py
+++ blogs/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import BlogPost
+
+def index(request):
+    """The home page for the blog"""
+    return render(request, 'blogs/index.html')
+
+def posts(request):
+    """Show all blogposts, abreviated"""
+    posts = BlogPost.objects.order_by('date_added')
+    context = {'posts': posts}
+    return render(request, 'blogs/posts.html', context)
+
+def post(request, post_id):
+    """Show single post"""
+    post = BlogPost.objects.get(id=post_id)
+    context = {'post': post}
+    return render(request, 'blogs/post.html', context)
