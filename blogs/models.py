08c37d96334ac717773154a8a12d10daa4a7d868
Renato Regalado <renato.regalado@gmail.com>
2020-05-21 11:15:18 -0500

    added a comment

--- blogs/models.py
+++ blogs/models.py
@@ -5,6 +5,7 @@ class BlogPost(models.Model):
     date_added = models.DateTimeField(auto_now_add=True)
     title = models.CharField(max_length=64, unique=True)
     text = models.TextField(max_length=400)
+    #remember to add a user
     
     def __str__(self):
         return f"{self.title}"
