89c56174de6cc8793ff237106c99bde3731d7ee5
Renato Regalado <renato.regalado@gmail.com>
2020-05-21 11:14:37 -0500

    added blogs app and created BlogPost model

---
+++ blogs/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class BlogPost(models.Model):
+    """An entry in a blog"""
+    date_added = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=64, unique=True)
+    text = models.TextField(max_length=400)
+    
+    def __str__(self):
+        return f"{self.title}"
